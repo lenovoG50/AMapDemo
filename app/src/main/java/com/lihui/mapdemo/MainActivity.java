a3a0bc526a853f7dc2f5cb9f8dfd7eb86ba98184
lenovoG50 <592465777@qq.com>
2017-09-26 10:47:39 +0800

    Initial commit

---
+++ app/src/main/java/com/lihui/mapdemo/MainActivity.java
@@ -0,0 +1,42 @@
+package com.lihui.mapdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.MapView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MapView mMapView;
+
+    private AMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mMapView = (MapView) findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);//此方法必须重写
+        mMap = mMapView.getMap();
+        mMap.setMapType(AMap.MAP_TYPE_NORMAL);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+}
