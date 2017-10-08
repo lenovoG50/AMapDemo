c1367545a72d37f24c551cf4a4a91a052afd29ff
lenovoG50 <592465777@qq.com>
2017-10-08 17:40:45 +0800

    定位

--- app/src/main/java/com/lihui/mapdemo/MainActivity.java
+++ app/src/main/java/com/lihui/mapdemo/MainActivity.java
@@ -1,16 +1,29 @@
 package com.lihui.mapdemo;
 
+import android.icu.text.SimpleDateFormat;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
 import com.amap.api.maps.AMap;
+import com.amap.api.maps.LocationSource;
 import com.amap.api.maps.MapView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity implements AMapLocationListener{
 
     private MapView mMapView;
 
     private AMap mMap;
+    public AMapLocationClient mapLocationClient;//声明mloacationClient对象
+    public AMapLocationClientOption mapLocationClientOption = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +33,28 @@ public class MainActivity extends AppCompatActivity {
         mMapView.onCreate(savedInstanceState);//此方法必须重写
         mMap = mMapView.getMap();
         mMap.setMapType(AMap.MAP_TYPE_NORMAL);
+
+        mapLocationClient = new AMapLocationClient(this);
+        //初始化定位参数
+        mapLocationClientOption = new AMapLocationClientOption();
+        //设置定位监听
+        mapLocationClient.setLocationListener(this);
+//设置定位模式为高精度模式，Battery_Saving为低功耗模式，Device_Sensors是仅设备模式
+        mapLocationClientOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+//设置定位间隔,单位毫秒,默认为2000ms
+        mapLocationClientOption.setInterval(2000);
+//设置定位参数
+        mapLocationClient.setLocationOption(mapLocationClientOption);
+// 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
+// 注意设置合适的定位时间的间隔（最小间隔支持为1000ms），并且在合适时间调用stopLocation()方法来取消定位请求
+// 在定位结束后，在合适的生命周期调用onDestroy()方法
+// 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
+//启动定位
+        mapLocationClient.startLocation();
+
     }
+
+
 
     @Override
     protected void onResume() {
@@ -38,5 +72,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLocationChanged(AMapLocation amapLocation) {
+        if (amapLocation != null) {
+            if (amapLocation.getErrorCode() == 0) {
+                //定位成功回调信息，设置相关消息
+                amapLocation.getLocationType();//获取当前定位结果来源，如网络定位结果，详见定位类型表
+                amapLocation.getLatitude();//获取纬度
+                amapLocation.getLongitude();//获取经度
+                amapLocation.getAccuracy();//获取精度信息
+                SimpleDateFormat df = null;
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+                    df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                }
+                Date date = new Date(amapLocation.getTime());
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    df.format(date);//定位时间
+                }
+            } else {
+                //显示错误信息ErrCode是错误码，errInfo是错误信息，详见错误码表。
+                Log.e("AmapError","location Error, ErrCode:"
+                        + amapLocation.getErrorCode() + ", errInfo:"
+                        + amapLocation.getErrorInfo());
+            }
+        }
     }
 }
